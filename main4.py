c04bbcb2b9fc1e8c8a999f0d7006f3d46c04e61a
jonesnil <jonesnil@iu.edu>
2019-09-23 17:16:32 -0400

    Edited a few main files to make random sprites appear and follow the mouse. Just a few examples of how sprites can be used in python arcade. I also made a 4th main file where zombies run from the mouse.

--- main4.py
+++ main4.py
@@ -16,14 +16,25 @@ class MyGame(arcade.Window):
         file_path = os.path.dirname(os.path.abspath(__file__))
         os.chdir(file_path)
         arcade.set_background_color(open_color.white)
-
+        self.character_list = arcade.SpriteList()
 
     def setup(self):
-        pass        
+        for i in range(20):
+            x = random.randint(0,800)
+            y = random.randint(0,600)
+            self.zombie_sprite = arcade.Sprite("assets/zombie.png", 1.5)
+            self.zombie_sprite.center_x = x
+            self.zombie_sprite.center_y = y
+            self.character_list.append(self.zombie_sprite)  
+        self.hero_sprite = arcade.Sprite("assets/head.png", 1.5)
+        self.hero_sprite.center_x = 400
+        self.hero_sprite.center_y = 300
+        self.character_list.append(self.hero_sprite) 
+
 
     def on_draw(self):
         arcade.start_render()
-        pass
+        self.character_list.draw()
 
 
     def update(self, delta_time):
@@ -31,7 +42,23 @@ class MyGame(arcade.Window):
 
 
     def on_mouse_motion(self, x, y, dx, dy):
-        pass
+        self.hero_sprite.center_x = x
+        self.hero_sprite.center_y = y
+        for zombie in self.character_list:
+            if(zombie.center_x >= 50):
+                if(x - zombie.center_x <= 50 and (x - zombie.center_x) > 0 and abs(y - zombie.center_y) <= 100 ):
+                    zombie.center_x -= 5
+            if(zombie.center_x <= 750):
+                if(zombie.center_x - x <= 50 and (zombie.center_x - x) > 0 and abs(zombie.center_y - y) <= 100 ):
+                    zombie.center_x += 5
+            if(zombie.center_y >= 45):
+                if(y - zombie.center_y <= 50 and (y - zombie.center_y) > 0 and abs(x - zombie.center_x) <= 100 ):
+                    zombie.center_y -= 5
+            if(zombie.center_y <= 555):
+                if(zombie.center_y - y <= 50 and (zombie.center_y - y) > 0 and abs(zombie.center_x - x) <= 100 ):
+                    zombie.center_y += 5
+
+
 
 def main():
     """ Main method """
